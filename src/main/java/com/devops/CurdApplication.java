c1455cdd286de276a4ae4ca4abd4bd23c3c6b686
Sk. M. Zaheer Abbas <zaheer.abbas1506@gmail.com>
2024-07-01 19:35:38 +0530

    added getmapping

--- src/main/java/com/devops/CurdApplication.java
+++ src/main/java/com/devops/CurdApplication.java
@@ -2,9 +2,18 @@ package com.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CurdApplication {
+	
+	@GetMapping("/") 
+	public String test() {
+		return "welcome to cicd";
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurdApplication.class, args);
